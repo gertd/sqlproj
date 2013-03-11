306172cdff2ba4f7dbf65b814e81d3d966153f18
Gert Drapers <gert.drapers@live.com>
2013-03-10 22:16:43 -0700

    SQLProj demonstrating how handle setting "table options"

---
+++ samples/SqlProjTableOptions/SqlProjTableOptions.sql
@@ -0,0 +1,24 @@
+﻿create table dbo.t1
+(
+         id int not null identity primary key clustered,
+         name nvarchar(100) not null
+);
+GO
+ALTER TABLE dbo.t1 SET (LOCK_ESCALATION = TABLE);
+GO
+create table dbo.t2
+(
+         id int not null identity primary key clustered,
+         name nvarchar(100) not null
+);
+GO
+ALTER TABLE dbo.t2 SET (LOCK_ESCALATION = AUTO);
+GO
+create table dbo.t3
+(
+         id int not null identity primary key clustered,
+         name nvarchar(100) not null
+);
+GO
+ALTER TABLE dbo.t3 SET (LOCK_ESCALATION = DISABLE);
+GO
