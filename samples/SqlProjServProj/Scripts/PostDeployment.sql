1d60237bb3840723cbd91d843ea455d66129d57f
gertd <gert.drapers@live.com>
2013-04-01 22:18:34 -0700

    SQL Project with server objects

---
+++ samples/SqlProjServProj/Scripts/PostDeployment.sql
@@ -0,0 +1,19 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+-- Move from permissions.sql
+-- SQL70018: Server level permissions are not allowed.
+IF EXISTS(SELECT * FROM sys.sql_logins WHERE name = N'login_test')
+BEGIN
+	GRANT VIEW SERVER STATE TO login_test;
+END
+GO
+
---
+++ samples/SqlProjServProj/Server Objects/connection_limit_trigger.sql
@@ -0,0 +1,11 @@
+﻿CREATE TRIGGER connection_limit_trigger
+ON ALL SERVER WITH EXECUTE AS 'login_test'
+FOR LOGON
+AS
+BEGIN
+IF ORIGINAL_LOGIN()= 'login_test' AND
+    (SELECT COUNT(*) FROM sys.dm_exec_sessions
+            WHERE is_user_process = 1 AND
+                original_login_name = 'login_test') > 3
+    ROLLBACK;
+END;
