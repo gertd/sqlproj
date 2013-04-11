f520f2f52735339e08fe02b125055a55f15b9d60
gertd <gert.drapers@live.com>
2013-04-11 13:32:56 -0700

    SQLProj database project references which accesses system objects in other database

---
+++ samples/SqlProjDBRefsWithSysRefs/ETL/ETL .sql
@@ -0,0 +1,10 @@
+﻿CREATE VIEW [DataWarehouseTableMetadata]
+AS 
+SELECT	t.[name],t.[_schema_name]
+FROM	(
+		SELECT	t.*,_schema_name = OBJECT_SCHEMA_NAME(t.[object_id], DB_ID('$(DataWarehouse)'))
+		FROM	[$(DataWarehouse)].[sys].[tables] t
+		)t
+CROSS APPLY [$(DataWarehouse)]..fn_listextendedproperty('@vWarehouseTableTypeExPropName',		'schema',_schema_name,'table',t.[name],default,default) ep_type
+CROSS APPLY [$(DataWarehouse)]..fn_listextendedproperty('@vDoCodeGenerationFlagExPropName',	'schema',_schema_name,'table',t.[name],default,default) ep_codegen
+GO
