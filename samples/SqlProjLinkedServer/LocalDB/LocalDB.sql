2e6e0d436541f53193a00182eb2ab16eb7b1af04
Gert Drapers <gert.drapers@live.com>
2013-04-29 22:13:14 -0700

    SqlProjLinkedServer - simple sample demonstrating using a Linked Server reference

---
+++ samples/SqlProjLinkedServer/LocalDB/LocalDB.sql
@@ -0,0 +1,12 @@
+﻿EXEC sp_addlinkedserver   
+   @server=N'RemoteServer', 
+   @srvproduct=N'',
+   @provider=N'SQLNCLI', 
+   @datasrc=N'localhost\sql2012';
+go
+
+create view dbo.SelectFromRemoteDB
+as
+select	*
+from	[$(RemoteServer)].[$(RemoteDB)].[RemoteSchema].[RemoteTable]
+go
